011bceef93afa2cbbcc10b9a2941f105de8f55e7
sravanikondari-cyber <sravanikondari@gmail.com>
2025-08-15 21:08:06 +0530

    Update day2work.python

--- day2work.py
+++ day2work.py
@@ -1,25 +1,27 @@
-'''n=int(input())
+#CHECK IF A NUMBER IS PRIME OR NOT.
+n=int(input())
 count=0
 for i in range(1,n+1):
     if(n%i==0):
         count+=1
-        
 if(count==2):
     print("Prime number")
 else:
-    print("Not Prime number")'''
+    print("Not Prime number")
 #Input:3
 #Output:Prime number
 
-'''n=int(input())
+#FIND THE FACTORIAL OF A NUMBER.
+n=int(input())
 fact=1
 for i in range(1,n+1):
     fact=fact*i
-print("factorial:",fact)'''
+print("factorial:",fact)
 #Input:6
 #Output:factorial: 720
 
-'''n=int(input())
+#PRINT THE FIBONACCI SERIES UP TO N TERMS.
+n=int(input())
 a=0
 b=1
 print(a,b,end='')
@@ -27,20 +29,22 @@ for i in range(2,n):
     c=a+b
     print(c,end='')
     a=b
-    b=c'''
+    b=c
 #Input:2
 #Output:0 1
 
-'''n=int(input())
+#FIND THE SUM OF DIGITS OF A GIVEN NUMBER.
+n=int(input())
 sum=0
 while n>0:
     rem=n%10
     sum=sum+rem
     n=n//10
-print("sum of digits:",sum)'''
+print("sum of digits:",sum)
 #Input:10
 #Output:sum of digit: 1
 
+#REVERSE THE DIGITS OF A GIVEN NUMBER
 n=int(input())
 rev=0
 while n>0:
@@ -51,3 +55,4 @@ print(rev)
 #Input:123
 #Output:321
     
+
